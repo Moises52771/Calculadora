8b6769116e747fb70979b12e818c67f266a2cfb2
Moises <moises.dev52771@gmail.com>
2022-03-02 18:11:39 -0300

    Add as operações

--- calculadora.js
+++ calculadora.js
@@ -11,15 +11,36 @@ onload = () => {
     document.querySelector('#bt-9').onclick = () => digito(9);
     document.querySelector('#bt-pt').onclick = virgula;
     document.querySelector('#bt-ac').onclick = limpa;
+    document.querySelector('#bt-divide').onclick = () => operador('/');
+    document.querySelector('#bt-mult').onclick = () => operador('*');
+    document.querySelector('#bt-sub').onclick = () => operador('-');
+    document.querySelector('#bt-som').onclick = () => operador('+');
+    document.querySelector('#bt-eq').onclick = calcula;
 }
 
 // variáveis para armazenar o valor, o operador e o estado da calculadora
 
 let sValor = '0';
 let ehNovoNumero = true;
+let valorAnterior = 0;
+let operacaoPendente = null;
+
 
 const atualizaVisor = () => {
-    document.querySelector('#display').innerText = sValor;
+    let [parteInteira, parteDecimal] = sValor.split(',');
+    let v = '';
+    let i = parteInteira.length - 1
+    c =  0;
+    for(i; i >= 0; i--){
+        if(++c > 3){
+            v = '.' + v;
+            c = 1;
+        }
+        v = parteInteira[i] + v;        
+    }
+    v += (parteDecimal ? ',' + parteDecimal : '');
+    document.querySelector('#display').innerText = v;
+
 }
 
 // tratamento do clique no botão de digito
@@ -55,6 +76,40 @@ const virgula = () => {
 // tratamento do clique no botão AC (all clear)
 const limpa = () => {
     ehNovoNumero = true;
+    valorAnterior = 0;
+    operacaoPendente = null;
     sValor = '0';
+    atualizaVisor();
+}
+
+const valorAtual = () => parseFloat(sValor.replace(',', '.'))
+
+const operador = (op) => {
+    calcula();
+    valorAnterior = valorAtual();
+    operacaoPendente = op;
+    ehNovoNumero = true;
+}
+
+const calcula = () => {
+    if (operacaoPendente != null){
+        let resultado;
+        console.log(valorAnterior + ' ' + operacaoPendente + ' ' + valorAtual());
+        switch(operacaoPendente){
+            case '+': resultado = valorAnterior + valorAtual();
+                break;
+            case '-': resultado = valorAnterior - valorAtual();
+                break;
+            case '/': resultado = valorAnterior / valorAtual();
+                break;
+            case '*': resultado = valorAnterior * valorAtual();
+                break;
+        }
+        sValor = resultado.toString().replace('.' , ',');
+    }
+
+    ehNovoNumero = true;
+    valorAnterior = 0;
+    operacaoPendente =  null;
     atualizaVisor();
 }
